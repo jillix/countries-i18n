2c6ead857f17d8ebfefe0c86cfe7db6468b47fbf
Ionică Bizău <bizauionica@yahoo.com>
2014-05-07 15:48:24 +0300

    Added a script that generates codes

---
+++ codes.js
@@ -0,0 +1,25 @@
+// Dependencies
+var Cheerio = require ("cheerio");
+var Request = require ("request");
+var fs = require ("fs");
+
+var teams = require ("./teams");
+var codes = {};
+
+var $ = Cheerio.load (fs.readFileSync ("./wiki-country-codes").toString());
+var $rows = $("tr", ".wikitable.sortable.jquery-tablesorter");
+
+for (var i = 1; i < $rows.length; ++i) {
+    codes[$("a", $rows.eq(i)).html()] = $("tt", $rows.eq(i)).html()
+}
+
+var teamCodes = {};
+for (var i = 0; i < teams.length; ++i) {
+    if (!codes[teams[i]]) {
+        console.warn ("Missing country code for: " + teams[i]);
+        continue;
+    }
+    teamCodes[teams[i]] = codes[teams[i]];
+}
+
+console.log (JSON.stringify (teamCodes, null, 4));
